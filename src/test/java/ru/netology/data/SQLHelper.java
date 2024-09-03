93e515683afaada0aa946b2eab97369bf898c055
Tyunat <020182tnm@gmail.com>
2024-09-03 16:27:57 +0300

    1commit

---
+++ src/test/java/ru/netology/data/SQLHelper.java
@@ -0,0 +1,45 @@
+package ru.netology.data;
+
+import com.mysql.cj.x.protobuf.MysqlxCrud;
+import lombok.SneakyThrows;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SQLHelper {
+    private static final QueryRunner QUERY_RUNNER = new QueryRunner();
+
+    private SQLHelper(){
+    }
+
+    private static Connection getConn() throws SQLException{
+        return DriverManager.getConnection(System.getProperty("db.url"), "app", "pass");
+    }
+
+    @SneakyThrows
+    public static DataHelper.VerificationCode getVerificationCode(){
+        var codeSQL = "select code from auth_codes order by created DESC limit 1";
+        var conn = getConn();
+        var code = QUERY_RUNNER.query(conn, codeSQL, new ScalarHandler<String>());
+        return new DataHelper.VerificationCode(code);
+    }
+
+    @SneakyThrows
+    public static void cleanDatabase(){
+        var connection = getConn();
+        QUERY_RUNNER.execute(connection, "DELETE FROM auth_codes");
+        QUERY_RUNNER.execute(connection, "DELETE FROM card_transactions");
+        QUERY_RUNNER.execute(connection, "DELETE FROM cards");
+        QUERY_RUNNER.execute(connection, "DELETE FROM users");
+    }
+
+    @SneakyThrows
+    public static void cleanAuthCodes(){
+        var connection = getConn();
+        QUERY_RUNNER.execute(connection, "DELETE FROM auth_codes");
+    }
+
+}
